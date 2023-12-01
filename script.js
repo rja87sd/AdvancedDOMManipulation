5d23a24568788bfa540838ac4bbfe529fb9c22c6
Ryan Arnold <rja87.sd@gmail.com>
2023-11-30 20:27:52 -0600

    When Button 1 is clicked, make a new List Item with text, 'New Item'

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 'use strict';
 
+// Grab elements by ID
 const mainContainer = document.getElementById('main-container');
 const listContainer = document.getElementById('list-container');
+// Grab all buttons using querySelectAll
+const buttons = document.querySelectorAll("button");
 
+// When Button 1 is clicked, make a new List Item with text, 'New Item'
+buttons[0].addEventListener("click", () => {
+  const ul = document.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = "New Item";
+  ul.appendChild(li);
+});

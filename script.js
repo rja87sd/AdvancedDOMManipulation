e22ac21fcf220012e0b735e326902e5cd382825f
Ryan Arnold <rja87.sd@gmail.com>
2023-11-30 20:53:15 -0600

    When Button 2 is clicked, change the text of all List Items to 'Updated Item'

--- script.js
+++ script.js
@@ -7,9 +7,19 @@ const listContainer = document.getElementById('list-container');
 const buttons = document.querySelectorAll("button");
 
 // When Button 1 is clicked, make a new List Item with text, 'New Item'
-buttons[0].addEventListener("click", () => {
+buttons[0].addEventListener('click', () => {
   const ul = document.querySelector("ul");
   const li = document.createElement("li");
   li.innerText = "New Item";
   ul.appendChild(li);
 });
+
+// When Button 2 is clicked, change the text of all List Items to 'Updated Item'
+buttons[1].addEventListener('click', () => {
+    const ulUpdate = document.querySelector('ul');
+    const liArray = document.querySelectorAll('li');
+    for (let i = 0; i < liArray.length; i++) {
+        liArray[i].innerText = 'Updated Item';
+    }
+});
+
